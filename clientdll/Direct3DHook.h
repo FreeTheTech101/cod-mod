f00fca9b66a7d504cd662c4200c77aba67eb9be8
Archon <archon432@gmail.com>
2014-03-18 22:34:18 +0100

    proper d3d9 proxy

---
+++ clientdll/Direct3DHook.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Exported function
+IDirect3D9* WINAPI Direct3DCreate9 (UINT SDKVersion);
+
+// regular functions
+void InitInstance(HANDLE hModule);
+void ExitInstance(void);
+void LoadOriginalDll(void);
