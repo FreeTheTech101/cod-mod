ad83ae67228269d531476c2aa04bcfe41ef94399
Maurice Heumann <MauriceHeumann@gmail.com>
2014-10-31 12:11:06 +0100

    Update SDLLP

--- code/SDLLP.cpp
+++ code/SDLLP.cpp
@@ -3,7 +3,83 @@
 // by momo5502
 // --------------------------------------+
 
-#include "SDLLP.h"
+#include <windows.h>
+#include <map>
+
+// Macro to declare an export
+// --------------------------------------+
+#define EXPORT(_export) extern "C" __declspec(naked) __declspec(dllexport) void _export() { SDLLP::GetExport(__FUNCTION__, LIBRARY); __asm { jmp eax } }  
+
+// Static class
+// --------------------------------------+
+class SDLLP
+{
+private:
+	static std::map<const char*, HINSTANCE> mLibraries;
+
+	static void	Log(const char* message, ...);
+	static void	LoadLibrary(const char* library);
+	static bool	IsLibraryLoaded(const char* library);
+
+public:
+	static FARPROC GetExport(const char* function, const char* library);
+};
+
+//	Class variable declarations
+// --------------------------------------+
+std::map<const char*, HINSTANCE> SDLLP::mLibraries;
+
+// Load necessary library
+// --------------------------------------+
+void SDLLP::LoadLibrary(const char* library)
+{
+	Log("[SDLLP] Loading library '%s'.", library);
+
+	CHAR mPath[MAX_PATH];
+
+	GetSystemDirectoryA(mPath, MAX_PATH);
+	strcat_s(mPath, "\\");
+	strcat_s(mPath, library);
+
+	mLibraries[library] = ::LoadLibraryA(mPath);
+
+	if(!IsLibraryLoaded(library)) Log("[SDLLP] Unable to load library '%s'.", library);
+}
+
+// Check if export already loaded
+// --------------------------------------+
+bool SDLLP::IsLibraryLoaded(const char* library)
+{
+	return (mLibraries.find(library) != mLibraries.end() && mLibraries[library]);
+}
+
+// Get export address
+// --------------------------------------+
+FARPROC SDLLP::GetExport(const char* function, const char* library)
+{
+	Log("[SDLLP] Export '%s' requested from %s.", function, library);
+
+	if(!IsLibraryLoaded(library)) LoadLibrary(library);
+
+	FARPROC address = GetProcAddress(mLibraries[library], function);
+
+	if(!address) Log("[SDLLP] Unable to load export '%s' from library '%s'.", function, library);
+	return address;
+}
+
+// Write debug string
+// --------------------------------------+
+void SDLLP::Log(const char* message, ...)
+{
+	CHAR buffer[1024];
+	va_list ap;
+
+	va_start(ap, message);
+	vsprintf(buffer, message, ap);
+	va_end(ap);
+
+	OutputDebugStringA(buffer);
+}
 
 // --------------------------------------+
 //	Adapt export functions and library
@@ -24,79 +100,3 @@ EXPORT(DebugSetLevel)
 EXPORT(DebugSetMute)
 EXPORT(Direct3DCreate9)
 EXPORT(Direct3DCreate9Ex)
-
-// --------------------------------------+
-//	DON'T TOUCH THE STUFF BELOW
-// --------------------------------------+
-
-std::map<const char*, FARPROC> SDLLP::mExports;
-HINSTANCE                      SDLLP::mHandle = NULL;
-
-// Load necessary library
-// --------------------------------------+
-void SDLLP::LoadLibrary()
-{
-	CHAR mPath[MAX_PATH];
-
-	GetSystemDirectoryA(mPath, MAX_PATH);
-	strcat_s(mPath, "\\" LIBRARY);
-
-	mHandle = ::LoadLibraryA(mPath);
-
-	if(!mHandle) Log("[SDLLP] Unable to load library '%s'.", LIBRARY);
-}
-
-// Check if export already loaded
-// --------------------------------------+
-bool SDLLP::IsExportLoaded(const char* function)
-{
-	return mExports.find(function) != mExports.end();
-}
-
-// Load export from dll
-// --------------------------------------+
-bool SDLLP::LoadExport(const char* function)
-{
-	Log("[SDLLP] Export '%s' requested.", function);
-
-	if(!mHandle) LoadLibrary();
-	if(IsExportLoaded(function)) return true;
-
-	FARPROC address = GetProcAddress(mHandle, function);
-
-	if(address)
-	{
-		mExports[function] = address;
-		return true;
-	}
-
-	Log("[SDLLP] Unable to load export '%s' from library '%s'.", function, LIBRARY);
-	return false;
-}
-
-// Return export address
-// --------------------------------------+
-FARPROC SDLLP::GetExport(const char* function)
-{
-	if(IsExportLoaded(function) || LoadExport(function))
-	{
-		return mExports[function];
-	}
-
-	Log("[SDLLP] Export '%s' not availabe.", function);
-	return NULL;
-}
-
-// Write debug string
-// --------------------------------------+
-void SDLLP::Log(const char* message, ...)
-{
-	CHAR buffer[1024];
-	va_list ap;
-
-	va_start(ap, message);
-	vsprintf(buffer, message, ap);
-	va_end(ap);
-
-	OutputDebugStringA(buffer);
-}
